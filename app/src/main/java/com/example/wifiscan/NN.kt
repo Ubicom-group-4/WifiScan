7d712645b72d49cf1ab3546141dfa8920d200151
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2021-04-29 14:23:56 +0200

    Refactor

--- app/src/main/java/com/example/wifiscan/NN.kt
+++ app/src/main/java/com/example/wifiscan/NN.kt
@@ -5,49 +5,39 @@ import kotlin.math.sqrt
 
 object NN {
 
-    private fun avg(list: List<Entry>, ssid: String): Int {
-        return list.mapNotNull { it.strengths.find { x -> x.first == ssid } }
+    private fun List<Entry>.avg(ssid: String) =
+        this.mapNotNull { it.strengths.find { x -> x.first == ssid } }
             .map { it.second }
             .average()
             .toInt()
-    }
 
-    private fun findOther(entry: Entry, entries: List<Entry>, ssid: String): Int {
-        return entry.strengths.find { it.first == ssid }?.second ?: avg(entries, ssid)
-    }
+    private fun Entry.findOther(entries: List<Entry>, ssid: String) =
+        this.strengths.find { it.first == ssid }?.second ?: entries.avg(ssid)
 
-    private fun distance(fst: Entry, snd: Entry, entries: List<Entry>): Double {
-
-        // dist = sqrt((ss1-e1)^2+(ss2-e2)^2+...+(ssm-em)^2)
-        val sum = fst.strengths.map { it.second - findOther(snd, entries, it.first) }
+    /**
+     * dist = sqrt((ss1-e1)^2+(ss2-e2)^2+...+(ssm-em)^2)
+     */
+    private fun Entry.distance(snd: Entry, entries: List<Entry>) = sqrt(
+        this.strengths.map { it.second - snd.findOther(entries, it.first) }
             .map { it.toDouble().pow(2.0) }
             .sum()
-
-        return sqrt(sum)
-    }
+    )
 
     fun nearestNeighbour(entries: List<Entry>, single: Entry): String {
-
-        val fixedValues = fixMissingValues(entries)
-
-        return fixedValues.map { Pair(it.location, distance(single, it, fixedValues)) }
+        val fixedValues = entries.fixMissingValues()
+        return fixedValues.map { Pair(it.location, single.distance(it, fixedValues)) }
             .minByOrNull { it.second }!!.first
     }
 
-    private fun fixMissingSSIDs(
-        strengths: List<Pair<String, Int>>,
-        ssids: HashSet<String>,
-        entries: List<Entry>
-    ): List<Pair<String, Int>> {
-        return ssids.map { strengths.find { x -> x.first == it } ?: Pair(it, avg(entries, it)) }
-    }
+    private fun Entry.fixMissingSSIDs(ssids: HashSet<String>, entries: List<Entry>) =
+        this.copy(strengths = ssids.map {
+            this.strengths.find { x -> x.first == it } ?: Pair(it, entries.avg(it))
+        })
 
-    private fun fixMissingValues(entries: List<Entry>): List<Entry> {
+    private fun List<Entry>.findSSIDs() =
+        this.flatMap { it.strengths }.map { it.first }.toHashSet()
 
-        val ssids = entries.flatMap { it.strengths }.map { it.first }.toHashSet()
-
-        return entries.map { it.copy(strengths = fixMissingSSIDs(it.strengths, ssids, entries)) }
-
-    }
+    private fun List<Entry>.fixMissingValues() =
+        this.map { it.fixMissingSSIDs(this.findSSIDs(), this) }
 }
 
